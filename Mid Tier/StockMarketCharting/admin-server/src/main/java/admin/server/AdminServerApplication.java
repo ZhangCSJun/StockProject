2c0bf4ab53f1cde84e5cdc9eddc1a4c6c3e66e00
ZhangCSJun <71885882@qq.com>
2020-06-15 23:15:12 +0800

    Milestone5
    Build test

--- Mid Tier/StockMarketCharting/admin-server/src/main/java/admin/server/AdminServerApplication.java
+++ Mid Tier/StockMarketCharting/admin-server/src/main/java/admin/server/AdminServerApplication.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminServerApplication extends SpringBootServletInitializer{
+public class AdminServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	    return builder.sources(AdminServerApplication.class);
-	}
-
 }
