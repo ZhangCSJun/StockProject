5acc5a9d363b384be3df1c1d1dc7b5e902d64c00
ZhangCSJun <71885882@qq.com>
2020-06-15 20:05:00 +0800

    Update for deploy

--- Mid Tier/StockMarketCharting/admin-server/src/main/java/admin/server/AdminServerApplication.java
+++ Mid Tier/StockMarketCharting/admin-server/src/main/java/admin/server/AdminServerApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminServerApplication {
+public class AdminServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(AdminServerApplication.class);
+	}
+
 }
